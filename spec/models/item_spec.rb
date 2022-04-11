c55b81f6f66c469dc0f63b445e39201bfae658ab
Kim G <xiaole.guo@yahoo.com>
2022-04-10 23:22:42 -0600

    add instance method selling_price in item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,4 +15,23 @@ RSpec.describe Item, type: :model do
     it {should validate_numericality_of(:unit_price).is_greater_than(0)}
   end
 
+
+  describe 'instance methods' do 
+
+    before (:each) do
+      @merchant1 = Merchant.create!(name: "Klein, Rempel and Jones")
+      @merchant2 = Merchant.create!(name: "Williamson Group")
+
+      @item1 = @merchant1.items.create!(name: "Item Ea Voluptatum", description: "A thing that does things", unit_price: 7654)
+      @item2 = @merchant1.items.create!(name: "Item Quo Magnam", description: "A thing that does nothing", unit_price: 10099)
+      @item3 = @merchant1.items.create!(name: "Item Voluptatem Sint", description: "A thing that does everything", unit_price: 8790)
+      @item4 = @merchant2.items.create!(name: "Item Rerum Est", description: "A thing that barks", unit_price: 3455)
+      @item5 = @merchant2.items.create!(name: "Item Itaque Consequatur", description: "A thing that makes noise", unit_price: 7900)
+    end
+    describe '.selling_price' do 
+      it 'returns selling_price as big decimal' do 
+        expect(@item1.selling_price).to eq(76.54.to_d)
+      end
+    end
+  end
 end
