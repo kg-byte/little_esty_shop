616d33caf9d023d57d54279cad5171b261499454
Kim G <xiaole.guo@yahoo.com>
2022-04-11 19:33:22 -0600

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'merchants/:id/dashboard', to: 'merchants#show'
   get 'merchants/:id/items', to: 'items#index'
   get 'merchants/:id/items/:item_id', to: 'items#show'
+  get 'merchants/:id/items/:item_id/edit', to: 'items#edit'
+  patch 'merchants/:merchant_id/items/:item_id', to: 'items#update'
   get 'merchants/:id/invoices', to: 'invoices#index'
+
 end
